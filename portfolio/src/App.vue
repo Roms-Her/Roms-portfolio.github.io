3af591f1d6bcaec66fac691960db522f1a54cf7d
romain hernandez <romsher.dev@gmail.com>
2024-02-20 22:30:22 +0100

    feat: mobile-first / nav/homePage/presentation

---
+++ portfolio/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+
+  <navbar />
+  <home-page />
+  <presentation-page />
+</template>
+
+
+<style>
+
+
+
+</style>
+
+
+<script setup>
+
+import navbar from './components/navbar_mobile.vue';
+import homePage from './components/homePage_mobile.vue';
+import presentationPage from './components/presentationPage.vue';
+
+</script>
