c16051936d7a6bdf710e63503e8882f341b61c8f
romain hernandez <romsher.dev@gmail.com>
2024-02-28 08:41:23 +0100

    test path 3

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio-roms/',
+  base: '/Roms-portfolio.github.io/',
   plugins: [
     vue(),
   ],
